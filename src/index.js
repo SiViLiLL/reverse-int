ea444367ac759ee23d1cbe00e634218dd106f308
Dilyara Arslanova <risharsivil@gmail.com>
2022-07-17 09:50:13 +0300

    without reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
-}
+	let arrNumber = String(Math.abs(n)).split('');
+	let a 
+	for (i = 0; i <= arrNumber.length - 1; i++){
+	    a = arrNumber[0];
+	    for (j = 1; j <= arrNumber.length - 1 - i; j++){
+	        arrNumber[j - 1] = arrNumber[j];
+	    }
+	    arrNumber[arrNumber.length - 1 - i] = a;
+	}
+	return parseInt(arrNumber.join(''))
+ }
